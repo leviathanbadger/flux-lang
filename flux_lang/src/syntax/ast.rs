0eae81c17c6db4f391939ef241bf8246ea5ac481
Brandon Slade <brandonyoyoslade@gmail.com>
2025-05-18 10:15:23 -0600

    test: add unit test for Program AST

--- flux_lang/src/syntax/ast.rs
+++ flux_lang/src/syntax/ast.rs
@@ -2,3 +2,14 @@
 
 #[derive(Debug, PartialEq)]
 pub struct Program;
+
+#[cfg(test)]
+mod tests {
+    use super::Program;
+
+    #[test]
+    fn debug_format() {
+        let prog = Program;
+        assert_eq!(format!("{prog:?}"), "Program");
+    }
+}
