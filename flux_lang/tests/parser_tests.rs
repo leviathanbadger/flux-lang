f6f95524cad99af64a8626fdb85a22a2ec793fe9
Brandon Slade <brandonyoyoslade@gmail.com>
2025-05-18 20:47:12 -0600

    Add structured parse errors

--- flux_lang/tests/parser_tests.rs
+++ flux_lang/tests/parser_tests.rs
@@ -12,3 +12,15 @@ fn parse_returns_ast() {
     let ast = parse_program("").expect("parse failure");
     assert_eq!(format!("{ast:?}"), "Program");
 }
+
+#[test]
+fn parse_error_reports_location() {
+    plugins::clear_plugins();
+    let err = parse_program("1").unwrap_err();
+    let pe = err
+        .downcast_ref::<flux_lang::syntax::ParseError>()
+        .expect("ParseError");
+    assert_eq!(pe.line, 0);
+    assert_eq!(pe.column, 0);
+    assert_eq!(pe.message, "invalid token");
+}
